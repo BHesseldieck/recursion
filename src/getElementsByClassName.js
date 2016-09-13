b3da2af25d2341ed8ec45bb2fe5a804d5989f805
Ben Hesseldieck <b.hesseldieck@gmail.com>
2016-09-12 20:15:08 -0700

    Update getElementsByClassName.js

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -10,13 +10,13 @@ var getElementsByClassName = function(className, ele) {
   ele = ele || document.children;
   for(var i = 0; i < ele.length; i++) {
     if(ele[i].className === className) {
-        console.log("ele I", ele[i]);
+        //console.log("ele I", ele[i]);
         result.push(ele[i]);
     }
     if(ele[i].childNodes.length > 0) {
       getElementsByClassName(className, ele[i].children);
     }
   }
-  console.log("result", result);
+  //console.log("result", result);
   return result;
 };
